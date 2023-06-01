51a4e4c98e754ce3a805f0ce29a49f7d0d4ab0d2
sintijablue <sintijablue@gmail.com>
2023-06-01 12:58:53 +0300

    NEW

--- app/src/androidTest/java/com/example/registrationform/EspressoTest.java
+++ app/src/androidTest/java/com/example/registrationform/EspressoTest.java
@@ -2,33 +2,25 @@ package com.example.registrationform;
 
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.replaceText;
+
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest {
+public class EspressoTest {
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
@@ -36,8 +28,8 @@ public class MainActivityTest {
 
     @Test
     public void mainActivityTest() {
-        onView(allOf (withId(R.id.edtTxtEmail), withText(“janisberzins@example.com”))) 
+        onView(allOf (withId(R.id.edtTxtEmail), withText("janisberzins@example.com")))
             .check(matches(isDisplayed()));
-        onView(withId(R.id.bntRegister)).perform(click());
+
     }
 }
